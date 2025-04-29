205f6212975e4d241a2c30b1737580cbd5fffcc3
fr34za <felipefreaza@gmail.com>
2025-04-29 18:09:05 -0300

    merkle pub fn commit

--- src/merkle.rs
+++ src/merkle.rs
@@ -1,38 +1,34 @@
-// Use sha256
+use sha2::digest::{generic_array::GenericArray, OutputSizeUser};
 use sha2::{Digest, Sha256};
 
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct Digest([u8; 32]);
+pub type HashDigest = GenericArray<u8, <Sha256 as OutputSizeUser>::OutputSize>;
 
 #[derive(Debug)]
 pub struct Merkle {
-    root: Digest,
-    layers: Vec<Vec<Digest>>,
+    pub root: HashDigest,
+    pub layers: Vec<Vec<HashDigest>>,
 }
 
 #[derive(Debug)]
 pub struct MerkleInclusionPath {
-    value: Vec<u8>,
-    path: Vec<(Digest, bool)>,
+    pub value: Vec<u8>,
+    pub path: Vec<(HashDigest, bool)>,
 }
 
 impl Merkle {
     pub fn commit(data: Vec<u8>) -> Merkle {
         assert!(
             data.len().is_power_of_two(),
-            "Data length must be power of two"
+            "Data length must be a power of two"
         );
 
-        let leaf_hashes = data
-            .chunks_exact(1)
-            .map(|chunk| Self::hash_leaf(chunk))
-            .collect::<Vec<_>>();
+        let leaf_hashes: Vec<HashDigest> = data.chunks_exact(1).map(Self::hash_leaf).collect();
 
-        let mut layers = vec![leaf_hashes];
+        let mut layers: Vec<Vec<HashDigest>> = vec![leaf_hashes];
 
         while layers.last().unwrap().len() > 1 {
             let current_layer = layers.last().unwrap();
-            let next_layer = current_layer
+            let next_layer: Vec<HashDigest> = current_layer
                 .chunks(2)
                 .map(|pair| Self::hash_node(&pair[0], &pair[1]))
                 .collect();
@@ -45,64 +41,16 @@ impl Merkle {
         }
     }
 
-    pub fn open(&self, index: usize) -> MerkleInclusionPath {
-        assert!(index < self.layers[0].len(), "Index out of bounds");
-
-        let mut path = Vec::new();
-        let mut current_index = index;
-        let value = self.layers[0][current_index].0.to_vec();
-
-        for layer in &self.layers[..self.layers.len() - 1] {
-            let sibling_index = if current_index % 2 == 0 {
-                current_index + 1
-            } else {
-                current_index - 1
-            };
-
-            let sibling = layer
-                .get(sibling_index)
-                .cloned()
-                .unwrap_or_else(|| Digest::default());
-
-            path.push((sibling, current_index % 2 == 0));
-            current_index /= 2;
-        }
-
-        MerkleInclusionPath { value, path }
-    }
-
-    fn hash_leaf(data: &[u8]) -> Digest {
+    fn hash_leaf(data: &[u8]) -> HashDigest {
         let mut hasher = Sha256::new();
         hasher.update(data);
-        Digest(hasher.finalize().into())
+        hasher.finalize()
     }
 
-    fn hash_node(left: &Digest, right: &Digest) -> Digest {
+    fn hash_node(left: &HashDigest, right: &HashDigest) -> HashDigest {
         let mut hasher = Sha256::new();
-        hasher.update(&left.0);
-        hasher.update(&right.0);
-        Digest(hasher.finalize().into())
-    }
-}
-
-impl MerkleInclusionPath {
-    pub fn verify(&self, root: &Digest) -> bool {
-        let mut current_hash = Self::hash_leaf(&self.value);
-
-        for (sibling_hash, is_right) in &self.path {
-            current_hash = if *is_right {
-                Merkle::hash_node(&current_hash, sibling_hash)
-            } else {
-                Merkle::hash_node(sibling_hash, &current_hash)
-            };
-        }
-
-        &current_hash == root
-    }
-}
-
-impl Default for Digest {
-    fn default() -> Self {
-        Digest([0u8; 32])
+        hasher.update(left);
+        hasher.update(right);
+        hasher.finalize()
     }
 }
