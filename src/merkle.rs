abf71f4d62a045ca3b056a4b808e07ce001ec22a
fr34za <felipefreaza@gmail.com>
2025-04-29 18:37:59 -0300

    fix up

--- src/merkle.rs
+++ src/merkle.rs
@@ -7,13 +7,11 @@ pub type HashDigest = GenericArray<u8, <Sha256 as OutputSizeUser>::OutputSize>;
 pub struct Merkle {
     pub root: HashDigest,
     pub layers: Vec<Vec<HashDigest>>,
+    pub data: Vec<u8>,
 }
 
 #[derive(Debug)]
-pub struct MerkleInclusionPath {
-    pub value: Vec<u8>,
-    pub path: Vec<(HashDigest, bool)>,
-}
+pub struct MerkleInclusionPath();
 
 impl Merkle {
     pub fn commit(data: Vec<u8>) -> Merkle {
@@ -22,32 +20,30 @@ impl Merkle {
             "Data length must be a power of two"
         );
 
-        let leaf_hashes: Vec<HashDigest> = data.chunks_exact(1).map(Self::hash_leaf).collect();
+        let first_layer: Vec<HashDigest> = data
+            .chunks_exact(2)
+            .map(|pair| Self::hash_node(&pair[0..1], &pair[1..2]))
+            .collect();
 
-        let mut layers: Vec<Vec<HashDigest>> = vec![leaf_hashes];
+        let mut layers: Vec<Vec<HashDigest>> = vec![first_layer];
 
         while layers.last().unwrap().len() > 1 {
             let current_layer = layers.last().unwrap();
             let next_layer: Vec<HashDigest> = current_layer
                 .chunks(2)
-                .map(|pair| Self::hash_node(&pair[0], &pair[1]))
+                .map(|pair| Self::hash_node(pair[0], pair[1]))
                 .collect();
             layers.push(next_layer);
         }
 
         Merkle {
-            root: layers.last().unwrap()[0].clone(),
+            root: layers.last().unwrap()[0],
             layers,
+            data,
         }
     }
 
-    fn hash_leaf(data: &[u8]) -> HashDigest {
-        let mut hasher = Sha256::new();
-        hasher.update(data);
-        hasher.finalize()
-    }
-
-    fn hash_node(left: &HashDigest, right: &HashDigest) -> HashDigest {
+    fn hash_node(left: impl AsRef<[u8]>, right: impl AsRef<[u8]>) -> HashDigest {
         let mut hasher = Sha256::new();
         hasher.update(left);
         hasher.update(right);
