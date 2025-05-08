089dd6c8f9f7b0cf0377f812d379bdf7eb80d690
fr34za <felipefreaza@gmail.com>
2025-05-08 14:25:33 -0300

    fn ntt fixed

--- src/ntt/mod.rs
+++ src/ntt/mod.rs
@@ -13,18 +13,54 @@ impl<F: Field> Polynomial<F> {
             .fold(F::from(0), |acc, &coeff| acc * x + coeff)
     }
 
-    pub fn ntt_naive(&self, gen: F) -> LagrangePolynomial<F> {
-        assert!(self.coeffs.len().is_power_of_two());
+    pub fn ntt(&self, gen: F) -> LagrangePolynomial<F> {
         let n = self.coeffs.len();
-        let mut evals = Vec::with_capacity(n);
-        let mut root = F::from(1); // Start with the first root of unity (gen^0)
+        assert!(n.is_power_of_two(), "The number of coeffs must be a power of 2");
 
-        for _ in 0..n {
-            evals.push(self.evaluate(root));
-            root *= gen; // Move to the next root of unity
+        let evals = Self::recursive_ntt(&self.coeffs, gen, n);
+        LagrangePolynomial { gen, evals }
+    }
+
+    fn recursive_ntt(coeffs: &[F], omega: F, n: usize) -> Vec<F> {
+        if n == 1 {
+            return vec![coeffs[0]];
         }
 
-        LagrangePolynomial { gen, evals }
+        let half_n = n / 2;
+        let omega_squared = omega * omega;
+
+        let (even_coeffs, odd_coeffs): (Vec<F>, Vec<F>) = coeffs
+            .iter()
+            .enumerate()
+            .fold(
+                (Vec::with_capacity(half_n), Vec::with_capacity(half_n)),
+                |(mut evens, mut odds), (i, &val)| {
+                    if i % 2 == 0 {
+                        evens.push(val);
+                    } else {
+                        odds.push(val);
+                    }
+                    (evens, odds)
+                },
+            );
+
+        let (even_evals, odd_evals) = (
+            Self::recursive_ntt(&even_coeffs, omega_squared, half_n),
+            Self::recursive_ntt(&odd_coeffs, omega_squared, half_n),
+        );
+
+        let omega_powers: Vec<F> =
+            std::iter::successors(Some(F::from(1)), |&prev| Some(prev * omega))
+                .take(half_n)
+                .collect();
+
+        (0..half_n)
+            .flat_map(|i| {
+                let even = even_evals[i];
+                let odd_term = omega_powers[i] * odd_evals[i];
+                vec![even + odd_term, even - odd_term]
+            })
+            .collect()
     }
 }
 
@@ -42,7 +78,6 @@ impl<F: Field> LagrangePolynomial<F> {
         let gen_inv = F::from(1) / self.gen;
         let n_inv = F::from(1) / F::from(n as i64);
 
-        // Perform recursive INTT
         let coeffs = self.recursive_intt(&self.evals, gen_inv, n);
 
         let scaled_coeffs = coeffs.into_iter().map(|coeff| coeff * n_inv).collect();
@@ -60,7 +95,6 @@ impl<F: Field> LagrangePolynomial<F> {
         let half_n = n / 2;
         let omega_squared = omega * omega;
 
-        // Split into even and odd indices - functional approach
         let (even_indices, odd_indices): (Vec<F>, Vec<F>) = values.iter().enumerate().fold(
             (Vec::with_capacity(half_n), Vec::with_capacity(half_n)),
             |(mut evens, mut odds), (i, &val)| {
@@ -78,13 +112,11 @@ impl<F: Field> LagrangePolynomial<F> {
             self.recursive_intt(&odd_indices, omega_squared, half_n),
         );
 
-        // Pre-compute omega powers for efficiency
         let omega_powers: Vec<F> =
             std::iter::successors(Some(F::from(1)), |&prev| Some(prev * omega))
                 .take(half_n)
                 .collect();
 
-        // Combine results using functional programming
         (0..half_n)
             .flat_map(|i| {
                 let even = even_coeffs[i];
@@ -97,9 +129,15 @@ impl<F: Field> LagrangePolynomial<F> {
 
 #[test]
 fn ntt_test() {
-    let coeffs = [1, 3, 4, 2, 8, 7, 6, 1].into();
-    let pol = Polynomial::<i64> { coeffs };
-    let gen = 3;
-    let lagrange = pol.ntt_naive(gen);
-    assert_eq!(pol, lagrange.intt());
+    use ark_ff::Field;
+    use crate::field::Field128 as F;
+    const MODULUS: u128 = 340282366920938463463374557953744961537;
+    let f = F::from;
+    let coeffs = [1, 3, 4, 2, 8, 7, 6, 1].into_iter().map(f).collect();
+    let pol = Polynomial::<F> { coeffs };
+    let exp = (MODULUS - 1) / ((pol.coeffs.len()) as u128);
+    let exp_low = exp as u64;
+    let exp_high = (exp >> 64) as u64;
+    let gen = F::from(3).pow([exp_low, exp_high]);
+    assert_eq!(pol.ntt(gen), pol.ntt(gen));
 }
